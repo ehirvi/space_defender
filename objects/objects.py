630241e131b3aac99ad28897d107955e5ab8c77c
ehirvi <eino.hirvi@gmail.com>
2024-05-08 18:05:12 +0300

    Resized and upscaled images

--- objects/objects.py
+++ objects/objects.py
@@ -3,8 +3,38 @@ import pygame
 class Player:
     def __init__(self, image: pygame.Surface, coords: tuple) -> None:
         self.image = image
-        self.width = self.image.get_width()
-        self.height = self.image.get_height()
+        self.size = self.image.get_size()
         self.coords = coords    # (x,y)
+        self.rect = self.image.get_rect()
         
         
+class Monster:
+    def __init__(self, image: pygame.Surface, coords: tuple) -> None:
+        self.image = image
+        self.size = self.image.get_size()
+        self.coords = coords
+        self.rect = self.image.get_rect()
+
+
+class PlayerMissile:
+    def __init__(self, image: pygame.Surface, coords: tuple) -> None:
+        self.image = image
+        self.size = self.image.get_size()
+        self.coords = coords
+        self.rect = self.image.get_rect()
+
+
+class MonsterMissile:
+    def __init__(self, image: pygame.Surface, coords: tuple) -> None:
+        self.image = image
+        self.size = self.image.get_size()
+        self.coords = coords
+        self.rect = self.image.get_rect()
+
+
+class Coin:
+    def __init__(self, image: pygame.Surface, coords: tuple) -> None:
+        self.image = image
+        self.size = self.image.get_size()
+        self.coords = coords
+        self.rect = self.image.get_rect()
